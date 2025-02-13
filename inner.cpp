dbb5c78c5b54c4e9d5d4351312a08ff9699b9122
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2025-02-13 12:04:45 -0600

    Create inner.cpp

---
+++ inner.cpp
@@ -0,0 +1,9 @@
+titleDirective:UKRAINE(slight);
+function():["TRIGGER"]*
+  TRIGGER():point.["battery":"wire"];
+TRIGGER(function(charge++));
+charge == "battery" + "wire";
+charge++ + charge++ + charge++;
+function(charge[0]);
+charge[1000];
+point.charge();
